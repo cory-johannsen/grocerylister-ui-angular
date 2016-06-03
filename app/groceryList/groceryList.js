27066fb77fd4ece8da5f65468d99da611b2ad38b
Cory Johannsen <cjohannsen@newrelic.com>
2016-06-02 22:47:27 -0700

    Basic list editing functional

--- app/groceryList/groceryList.js
+++ app/groceryList/groceryList.js
@@ -10,49 +10,78 @@ angular.module('groceryLister.groceryList', ['ngRoute'])
 }])
 
 .filter('unassignedProducts', function() {
-      return function (products, groceryList) {
-        var out = []
+  return function (products) {
+    var availableProducts = []
 
-        // Using the angular.forEach method, go through the array of data and perform the operation of figuring out if the language is statically or dynamically typed.
-        angular.forEach(products, function(product) {
+    angular.forEach(products, function(product) {
 
-          if (groceryList.indexOf(product) == -1) {
-            out.push(product)
-          }
+    //  if (assignedProducts.indexOf(product) == -1) {
+        availableProducts.push(product)
+    //  }
 
-        })
+    })
 
-        return out
-      }
-    }
-)
+    return availableProducts
+  }
+})
 
 .controller('GroceryListCtrl',  ['$scope', '$http', function($scope, $http) {
   $scope.assignProduct = function(product) {
-    $scope.groceryList.products.push(product);
-    var response = $http.get("http://localhost:9000/grooceryList/", $scope.groceryList.id, "/products/" )
-    response.success(function(data, status, headers, config) {
-      $scope.groceryList = data
-    })
-    response.error(function(data, status, headers, config) {
-      console.log("GET error fetching grocery lists: " + data)
-    })
+
+    var response = $http({
+      method: 'GET',
+      url: $scope.groceryList._links.products.href,
+      headers: {
+        'Content-Type': 'text/uri-list'
+      },
+      data: $scope.assignedProducts
+    }).then(
+        function(data, status, headers, config) {
+          console.log("POST successful adding product ", product.name, " to grocery list ", $scope.groceryList.name)
+          loadGroceryList($http, $scope)
+        },
+        function(data, status, headers, config) {
+          console.log("POST adding product ", product.name, " to grocery list ", $scope.groceryList.name, data)
+        }
+    )
 
   }
 
-  var response = $http.get("http://localhost:9000/current_grocery_list")
-  response.success(function(data, status, headers, config) {
-    $scope.groceryList = data
-  })
-  response.error(function(data, status, headers, config) {
-    console.log("GET error fetching grocery lists: " + data)
-  })
+  loadGroceryList($http, $scope)
+  loadAvailableProducts($http, $scope)
 
-  response = $http.get("http://localhost:9000/product")
-  response.success(function(data, status, headers, config) {
-    $scope.productList = data._embedded.product
-  })
-  response.error(function(data, status, headers, config) {
-    console.log("GET error fetching grocery lists: " + data)
-  })
 }])
+
+function loadGroceryList(http, scope) {
+  http.get("http://localhost:9000/grocerylist?sort=lastModified&size=1")
+      .then(
+          function(data, status, headers, config) {
+            scope.groceryList = data.data._embedded.groceryList[0]
+            loadAssignedProducts(http, scope, scope.groceryList)
+          },
+          function(data, status, headers, config) {
+            console.log("GET error fetching grocery list: ", data)
+          })
+}
+
+function loadAssignedProducts(http, scope, groceryList) {
+  http.get(groceryList._links.products.href)
+      .then(
+          function(data, status, headers, config) {
+            scope.assignedProducts = data.data
+          },
+          function(data, status, headers, config) {
+            console.log("GET error fetching products for grocery list: ", groceryList.name)
+          })
+}
+
+function loadAvailableProducts(http, scope) {
+  http.get("http://localhost:9000/product")
+      .then(
+          function(data, status, headers, config) {
+            scope.availableProducts = data.data
+          },
+          function(data, status, headers, config) {
+            console.log("GET error fetching available products: ", data)
+          })
+}
